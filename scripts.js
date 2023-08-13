87b47223d335031b7962d3cad26a85b36c188088
kikisnetservice <142000340+kikisnetservice@users.noreply.github.com>
2023-08-13 11:57:52 +0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".nav-links a");
   const ctaButton = document.querySelector(".cta-button");
+  const navbar = document.querySelector(".navbar");
 
   navLinks.forEach(link => {
     link.addEventListener("click", smoothScroll);
@@ -13,9 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const targetId = event.currentTarget.getAttribute("href");
     const targetSection = document.querySelector(targetId);
     
-    window.scrollTo({
-      top: targetSection.offsetTop - 40,
-      behavior: "smooth"
-    });
+	if (targetSection) {
+	  window.scrollTo({
+        top: targetSection.offsetTop - 40,
+        behavior: "smooth"
+      });
+	}
   }
+  
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      navbar.classList.add("navbar-fixed");
+    } else {
+      navbar.classList.remove("navbar-fixed");
+    }
+  });
 });
