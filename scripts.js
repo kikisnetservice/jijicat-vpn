0b17377e8f5568db623f3050d186c28155cc35e0
kikisnetservice <142000340+kikisnetservice@users.noreply.github.com>
2023-08-13 10:43:54 +0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".nav-links a");
+  const ctaButton = document.querySelector(".cta-button");
 
   navLinks.forEach(link => {
     link.addEventListener("click", smoothScroll);
   });
+  
+  ctaButton.addEventListener("click", smoothScroll);
 
   function smoothScroll(event) {
     event.preventDefault();
