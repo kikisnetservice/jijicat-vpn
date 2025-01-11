fb9debed9fc74d0dcfa1c6dfa4c17bf1fb14c569
kikisnetservice <142000340+kikisnetservice@users.noreply.github.com>
2025-01-11 19:10:39 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".nav-links a");
   const ctaButton = document.querySelector(".cta-button");
   const navbar = document.querySelector(".navbar");
+  const sections = document.querySelectorAll(".section");
 
   navLinks.forEach(link => {
     link.addEventListener("click", smoothScroll);
@@ -14,40 +15,39 @@ document.addEventListener("DOMContentLoaded", function () {
     const targetId = event.currentTarget.getAttribute("href");
     const targetSection = document.querySelector(targetId);
     
-	if (targetSection) {
-	  window.scrollTo({
+    if (targetSection) {
+      window.scrollTo({
         top: targetSection.offsetTop - 40,
         behavior: "smooth"
       });
-	}
+    }
   }
-  
+
   window.addEventListener("scroll", function () {
+    // Добавление фиксированного navbar при скролле
     if (window.scrollY > 0) {
       navbar.classList.add("navbar-fixed");
     } else {
       navbar.classList.remove("navbar-fixed");
     }
-  });
-  
-  // Функция, которая убирает hover-состояние
-  function removeHover() {
-    const navLinks = document.querySelectorAll('.nav-links a');
-    navLinks.forEach(link => {
-      link.classList.remove('hover');
-    });
-  }
-  
-  // Слушаем событие скролла на документе
-  document.addEventListener('scroll', removeHover);
 
-  // Слушаем событие касания (для мобильных устройств)
-  document.addEventListener('touchstart', removeHover);
-  
-  // Также добавляем "hover" при касании ссылки
-  document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('touchstart', function() {
-      link.classList.add('hover');
+    // Определяем текущую видимую секцию
+    let currentSection = "";
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 50;  // Делаем немного больше отступ для фиксированного меню
+      const sectionBottom = sectionTop + section.offsetHeight;
+
+      if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+        currentSection = section.getAttribute("id");
+      }
+    });
+
+    // Обновление активной ссылки
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href").substring(1) === currentSection) {
+        link.classList.add("active");
+      }
     });
   });
 });
